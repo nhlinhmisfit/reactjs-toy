449272490c05f8f41ec08932804f390bc34afbf8
nhlinhmisfit <nhlinh@misfit.com>
2016-03-24 14:21:01 +0700

    state

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 class App extends React.Component {
+	constructor () {
+		super();
+		this.state = {
+			txt: 'this is the state txt',
+			cat: 0
+		}
+	}
+	update(e) {
+		this.setState({txt: e.target.value})
+	}
 	render() {
 		return (
-			<div>Hello World </div>
+			<div>
+				<input type="text" 
+					onChange={this.update.bind(this)} />
+				<h1>{this.state.txt} </h1>
+			</div>
 		);
 	}
 }
